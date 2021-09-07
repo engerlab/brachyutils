114659e2708a7f319be928660ed554d7b9527171
hosseinjafar <hosseinj.2555@gmail.com>
2021-09-06 22:55:46 -0400

    reading 3 dose file is fine, getting started on dicompyle

--- check_3ddose.py
+++ check_3ddose.py
@@ -1,22 +1,34 @@
 # needed libraries
 import dose_utils
 import os
+import numpy as np
+import dicompylercore
 
 # set directory of the 3ddose files
-fileDir = "/home/hosseinj/data/Case1_elekta_simResults"
+simfileDir = "/home/hosseinj/data/Case1_elekta_simResults"
+dicomfileDir = ""
+
 
 # a for loop to iterate through dose files in the specified directory
-for fileName in os.listdir(fileDir):
+for fileName in os.listdir(simfileDir):
 # only pick .3ddose files
     if fileName.endswith(".3ddose"):
 # get and print the full path of the file
-        doseFile = os.path.join(fileDir, fileName)
+        doseFile = os.path.join(simfileDir, fileName)
         print(doseFile)
 # extract the dose data out of the .3ddose file
         dose_array = dose_utils.load_3ddose(doseFile)["grid"]
-        print("Type of the dose_array is:")
-        print(type(dose_array))
-        print("\n")
+        print("Type of the dose_array is: ", type(dose_array))
+        print("-----------------")
+        print("dimensions of the dose_array is:", np.shape(dose_array))
+        print("-----------------")
+
+# load the dicomfiles 
+
+
+
+
+
 
 
 
