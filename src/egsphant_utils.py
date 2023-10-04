39aef03c4d17d98f14ee88f28d9d4335ae41766b
hosseinjafar <hosseinj.2555@gmail.com>
2023-10-04 19:21:45 -0400

    fixed a bunch of bugs, wrote the equality function and working on write_to_egsphant

--- src/egsphant_utils.py
+++ src/egsphant_utils.py
@@ -34,7 +34,7 @@ import pydicom
 
 import json
 
-class BrachyEgsphant():
+class BrachyEgsphant:
     r"""
     Purpose:
         An object to allow for loading and manipulating the .egsphant files
@@ -59,7 +59,9 @@ class BrachyEgsphant():
         - write_to_nrrd()
         - crop_by_index()
         - crop_by_body_contour()
-        
+        - assert_BrachyEgsphant_notEmpty()
+        - info()
+        - is_equal()
     
     Dependencies:
         numpy
@@ -91,9 +93,19 @@ class BrachyEgsphant():
     axis:np.ndarray
     _sanity_axis:np.ndarray
     
-    def __init__(self, pth_egsphant_file:str):
-        if pth_to_load is not None:
-            self.load_file_to_BrachyEgsphant(pth_egsphant_file) 
+    def __init__(self, pth_egsphant_file:Optional[str]=None):
+        if pth_egsphant_file is not None:
+            self.load_file_to_BrachyEgsphant(pth_egsphant_file)
+        else:
+            self.material_matrix:np.ndarray = None
+            self.density_matrix:np.ndarray = None
+            self.num_materials:int = None
+            self.material_dict:dict = {}
+            self.num_voxels:np.ndarray = None
+            self.vox_size:np.ndarray = None
+            self.topleft:np.ndarray = None
+            self.axis:np.ndarray = None
+            self._sanity_axis:np.ndarray = None
     
     def load_file_to_BrachyEgsphant(self, pth_egsphant_file):
         pth_egsphant_file = os.path.abspath(pth_egsphant_file)
@@ -125,14 +137,16 @@ class BrachyEgsphant():
             for i in range(self.num_materials):
                 self.material_dict[egsphant.readline().strip()] = i
             
+            egsphant.readline()
+            
             # load number of voxels
             self.num_voxels = np.array([int(i) for i in egsphant.readline().strip().split()])
             
             # load the axis grid points
             self._sanity_axis = np.array([
-                [float(x) for x in egsphant.readline().strip().split()],
-                [float(y) for y in egsphant.readline().strip().split()],
-                [float(z) for z in egsphant.readline().strip().split()]
+                np.array([float(x) for x in egsphant.readline().strip().split()], dtype=np.float32),
+                np.array([float(y) for y in egsphant.readline().strip().split()], dtype=np.float32),
+                np.array([float(z) for z in egsphant.readline().strip().split()], dtype=np.float32)
                 ], dtype=object)
             self._sanity_axis = np.flip(self._sanity_axis, axis=0)
             
@@ -152,11 +166,11 @@ class BrachyEgsphant():
             )
             
             self.axis = self.calculateAxis()
-            assert np.array_equal(np.concatenate(self.axis), np.concatenate(self._sanity_axis)), "axis is not the same"
+            assert np.isclose(np.concatenate(self.axis), np.concatenate(self._sanity_axis), rtol=1e-3).all(), "axis is not the same"
  
             # prepare empty matricies to hold material and density images
-            self.material_matrix = npzeros((self.num_voxels[2], self.num_voxels[1], self.num_voxels[0]), dtype=np.int)
-            self.density_matrix = npzeros((self.num_voxels[2], self.num_voxels[1], self.num_voxels[0]), dtype=np.int)
+            self.material_matrix = npzeros((self.num_voxels[2], self.num_voxels[1], self.num_voxels[0]), dtype=int)
+            self.density_matrix = npzeros((self.num_voxels[2], self.num_voxels[1], self.num_voxels[0]), dtype=np.float32)
 
             # load the material composition data in to the matrix
             for k in range(self.num_voxels[2]):
@@ -194,9 +208,89 @@ class BrachyEgsphant():
         
         return axes
     
+    def write_to_ctegsphant(self, fileName:str):
+        r''' 
+        Purpose: 
+            This function will write the contents of a BrachyEgsphant onto a text file with .egsphant extension. 
+        
+        inputs:
+            - self := a BrachyEgsphant object containing the following keys:
+                num_materials:int
+                material_dict:dict
+                num_voxels:np.ndarray       [x, y, z]
+                vox_size:np.ndarray         #Not Written
+                topleft:np.ndarray          #Not Written
+                axis:np.ndarray             [z, y, x] -> [x, y, z]
+                material_matrix:np.ndarray  [z, y, x] -> [x, y, z]
+                density_matrix:np.ndarray   [z, y, x] -> [x, y, z]
                 
+            - fileName := the directory path where the file will be written
+        '''   
+        fileName = os.path.abspath(fileName)
+        num_materials = str(self.num_materials) + '\n'
+        materials = ' '.join(map(str, self.material_dict))
+        dimensions = ' '.join(map(str, self.num_voxels.astype(int))) + '\n'
+        x_axis = ' '.join(map(str, self.axis[2])) + '\n'
+        y_axis = ' '.join(map(str, self.axis[1])) + '\n'
+        z_axis = ' '.join(map(str, self.axis[0])) + '\n'
+        dose_flattened = ' '.join(map(str, self.grid.flatten('C'))) + '\n'
+        if self.uncertainty is not None:
+            uncertainty_flattened = ' '.join(map(str, self.uncertainty.flatten('C'))) + '\n'
+        else:
+            uncertainty_flattened = ''
             
-            
+        with open(fileName, 'w') as file:
+            lines = [dimensions, x_axis, y_axis, z_axis, dose_flattened, uncertainty_flattened]
+            file.writelines(lines)
+    
+    def is_equal(self, new_BrachyEgsphant:BrachyEgsphant):
+        r"""
+        Purpose:
+            To compare if self:BrachyDose has the same attributes as an input BrachyDose
+        
+        Inputs:
+            - new_brachyDose: another BrachyDose object whose attributes may or may not contain equal info as the attributes of self. 
+        
+        Outputs:
+            True if attributes of new_brachyDose are the same as self
+            False otherwise
+        """
+        assert isinstance(new_BrachyEgsphant, BrachyDose), "input must be of type BrachyDose"
+        assert np.array_equal(self.material_matrix, new_BrachyEgsphant.material_matrix), "material matrix is not the same" 
+        assert np.array_equal(self.density_matrix, new_BrachyEgsphant.density_matrix), "density matrix is not the same" 
+        assert np.isclose(np.concatenate(self.axis), np.concatenate(new_BrachyEgsphant.axis), rtol=1e-3).all(), "axis is not the same"
+        assert np.array_equal(self.num_materials, new_BrachyEgsphant.num_materials), "number of materials is not the same"
+        assert self.material_dict == new_BrachyEgsphant.material_dict, "the material dictionary is not the same"
+        assert np.array_equal(self.num_voxels, new_BrachyEgsphant.num_voxels), "num_voxels is not the same"
+        assert np.array_equal(self.vox_size, new_BrachyEgsphant.vox_size), "vox_size is not the same"
+        assert np.array_equal(self.topleft, new_BrachyEgsphant.topleft), "topleft is not the same"
+        
+        
+        return np.array_equal(self.material_matrix, new_BrachyEgsphant.material_matrix) \
+            and np.array_equal(self.density_matrix, new_BrachyEgsphant.density_matrix) \
+            and np.isclose(np.concatenate(self.axis), np.concatenate(new_BrachyEgsphant.axis), rtol=1e-3).all() \
+            and np.array_equal(self.num_materials, new_BrachyEgsphant.num_materials) \
+            and self.material_dict == new_BrachyEgsphant.material_dict \
+            and np.array_equal(self.num_voxels, new_BrachyEgsphant.num_voxels) \
+            and np.array_equal(self.vox_size, new_BrachyEgsphant.vox_size) \
+            and np.array_equal(self.topleft, new_BrachyEgsphant.topleft)
+      
+         
+    def assert_BrachyEgsphant_notEmpty(self):
+        r"""
+        Purpose:
+            to see which field of a brachyEgsphant object is empty
+        """
+        assert self.material_matrix is not None, "error: material_matrix is None"
+        assert self.density_matrix is not None, "error: density_matrix is None"
+        assert self.num_materials is not None, "error: num_materials is None"
+        assert self.material_dict is not None, "error: material_dict is None"
+        assert self.num_voxels is not None, "error: num_voxels is None"
+        assert self.vox_size is not None, "error: vox_size is None"
+        assert self.topleft is not None, "error: topleft is None"
+        assert self.axis is not None, "error: axis is None"
+        
+        
 def load_egsphant(filename):
     phant = {}
     with open(filename, "r") as egsphant:
@@ -227,3 +321,30 @@ def load_egsphant(filename):
             egsphant.readline()
 
     return phant
+
+def test_write_to_egsphant():
+    pth_input = "../data_test/glen_prostate_p1_3mm_ct.egsphant"
+    pth_output = os.path.dirname(pth_input) + "test_"+os.path.basename(pth_input)
+    
+    egsphant_obj = BrachyEgsphant()
+    egsphant_obj.load_from_ctegsphant(pth_input)
+    egsphant_obj.assert_BrachyEgsphant_notEmpty()
+    
+    egsphant_obj.write_to_ctegsphant(pth_output)
+    new_egsphant_obj = BrachyEgsphant()
+    new_egsphant_obj.load_from_ctegsphant(pth_output)
+    
+    egsphant_obj.is_equal(new_egsphant_obj)
+    
+
+def test_load_from_ctegsphant():
+    pth_input = "../data_test/glen_prostate_p1_3mm_ct.egsphant"
+    
+    egsphant_obj = BrachyEgsphant()
+    egsphant_obj.load_from_ctegsphant(pth_input)
+    egsphant_obj.assert_BrachyEgsphant_notEmpty()
+
+if __name__=="__main__":
+    
+    # running tests top is the latest test written
+    test_load_from_ctegsphant()
