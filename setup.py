d04251c38e9324416072db465d1b30c0985f7337
hosseinjafar <hosseinj.2555@gmail.com>
2023-10-06 21:01:38 -0400

    fucking around to find out

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='brachyutils',
       version='1.0',
       description='Python utility packages for handling dose files and egsphant files.',
       author='EngerLab',
-      packages = find_packages(include=['brachyutils', 'src.*']),
+      packages = find_packages('src'),
       install_requires=[
         "contourpy",
         "cycler",
