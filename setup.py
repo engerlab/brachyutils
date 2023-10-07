857518905abff7217dd9d2814cd231111e52a6b2
hosseinjafar <hosseinj.2555@gmail.com>
2023-10-06 21:08:08 -0400

    fkn arnd 2 fnd out

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='brachyutils',
       version='1.0',
       description='Python utility packages for handling dose files and egsphant files.',
       author='EngerLab',
-      packages = find_packages('brachyutils'),
+      packages = find_packages('.'),
       install_requires=[
         "contourpy",
         "cycler",
