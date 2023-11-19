d07f49e7a1bb1e04c089157e97410689f65cb7eb
hosseinjafar <hosseinj.2555@gmail.com>
2023-11-19 12:25:00 -0500

    implemented function in dicom utils to obtain the mask of each structure

--- brachyutils/src/dicom_utils.py
+++ brachyutils/src/dicom_utils.py
@@ -70,8 +70,34 @@ def get_strcuture_mask_from_dicom(pth_dir_dicom:str, query_structure_list:list):
             it should contain both images and RTSTRUCT file
         - query_structure_list := list of structure names to find the mask of.
     Outputs:
-    
+        
     """
+    pth_dir_dicom = os.path.abspath(pth_dir_dicom)
+    assert os.path.exists(pth_dir_dicom), "given dicom path does not exist"
+    assert glob(pth_dir_dicom+"/*.dcm"), "there are no dicom files in this directory"
+    
+    # load the structure file into an rt_struct object
+    dicom_reader = DicomReaderWriter(description=f"getting structure masks", arg_max=True)
+    dicom_reader.walk_through_folders(pth_dir_dicom)
+    all_rois = dicom_reader.return_rois()
+
+    result_dict = {}
+
+    for query_structure_name in query_structure_list:
+        # # find the name of the body structure inside the rt_structure object
+        dicom_structure_name = [name for name in all_rois if query_structure_name in name.lower()]
+        
+        # # get the numpy array of the body structure:
+        assert len(dicom_structure_name) >= 1, "body contour not found!"
+        dicom_reader.set_contour_names_and_associations(contour_names=dicom_structure_name)
+        
+        dicom_reader.get_mask()
+        mask_numpy = dicom_reader.mask
+        result_dict[query_structure_name] = mask_numpy
+    
+    return result_dict
+        
+
 
 
 def test_get_structure_index_range():
@@ -79,10 +105,13 @@ def test_get_structure_index_range():
     # pth_3ddose = "../../data_test/run_1_glen_prostate_p1.3ddose"
     print(get_structure_index_range(pth_dicomRS, ['body', 'urethra', 'rectum', 'ctv']))
     # print(get_structure_index_range(pth_dicomRS, ['body']))
-    
+
+def test_get_strcuture_mask_from_dicom():
+    pth_dicomRS = "../../data_test/prostate-glen-p1-dcm/"
+    get_strcuture_mask_from_dicom(pth_dicomRS, ['urethra', 'rectum', 'ctv'])
 
 if __name__ == "__main__":
     # app()
     # a Test for the following functions
-    test_get_structure_index_range()
-    # test_get_body_contour_range_from_many_patients_dicom()
+    # test_get_structure_index_range()
+    test_get_strcuture_mask_from_dicom()
