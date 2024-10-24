f2218512c06c6a2545ea4fa21c9cb6b56ee5291d
hosseinjafar <hosseinj.2555@gmail.com>
2024-10-24 19:58:47 -0400

    applied linting to a tester file

--- brachyutils/tests/test_geometry_utils.py
+++ brachyutils/tests/test_geometry_utils.py
@@ -1,38 +1,40 @@
-from brachyutils import BrachyPhantom
 from glob import glob
-from pathlib import Path
+# from pathlib import Path
+
 import numpy as np
 
+from brachyutils import BrachyPhantom
+
+
 def test_brachy_phantom():
-    pth_dicom = "../data_test/prostate-glen-p1-dcm" 
+    # pth_dicom = "../data_test/prostate-glen-p1-dcm"
     pth_nrrd = "../data_test/prostate_glen_p1_ct.nrrd"
-    pth_structure = glob(pth_dicom+"/RS*.dcm")[0]
+    # pth_structure = glob(pth_dicom + "/RS*.dcm")[0]
     phantom_obj = BrachyPhantom(
         # dir_dicom=pth_dicom,
         pth_phantom_file=pth_nrrd,
         # pth_structures_file=pth_structure
-        )
+    )
     phantom_obj.info()
+
 
 def test_get_structure_mask():
     pth_dicom = "../data_test/prostate-glen-p1-dcm"
-    pth_structure = glob(pth_dicom+"/RS*.dcm")[0]
-    phantom_obj = BrachyPhantom(
-        dir_dicom=pth_dicom,
-        pth_structures_file=pth_structure
-        )
-    print(phantom_obj.get_structure_mask(['ctv'], mask_type=np.ndarray))
+    pth_structure = glob(pth_dicom + "/RS*.dcm")[0]
+    phantom_obj = BrachyPhantom(dir_dicom=pth_dicom, pth_structures_file=pth_structure)
+    print(phantom_obj.get_structure_mask(["ctv"], mask_type=np.ndarray))
+
 
 def test_write_image_to_dicom():
     pth_dicom = "../data_test/prostate-glen-p1-dcm"
     pth_nrrd = "../data_test/prostate_glen_p1_ct.nrrd"
-    pth_structure = glob(pth_dicom+"/RS*.dcm")[0]
+    pth_structure = glob(pth_dicom + "/RS*.dcm")[0]
     pth_out = "../data_test/test_export_plan/test_p1_ct"
     phantom_obj = BrachyPhantom(
         # dir_dicom=pth_dicom,
         pth_phantom_file=pth_nrrd,
-        # pth_structures_file=pth_structure
-        )
+        pth_structures_file=pth_structure
+    )
     phantom_obj.write_image_to_dicom(pth_out)
 
     new_phantom = BrachyPhantom(
@@ -40,47 +42,43 @@ def test_write_image_to_dicom():
     )
     new_phantom.is_equal(phantom_obj)
 
+
 def test_write_image_to_nrrd():
     pth_dicom = "../data_test/prostate-glen-p1-dcm"
     pth_out = "../data_test/prostate_glen_p1_ct.nrrd"
     phantom_obj = BrachyPhantom(dir_dicom=pth_dicom)
     phantom_obj.write_image_to_nrrd(pth_out)
 
+
 def test_write_structures_to_nrrd():
     pth_dicom = "../data_test/prostate-glen-p1-dcm"
-    pth_structure = glob(pth_dicom+"/RS*.dcm")[0]
+    pth_structure = glob(pth_dicom + "/RS*.dcm")[0]
     pth_out = "../data_test/prostate_glen_p1_structs.seg.nrrd"
-    phantom_obj = BrachyPhantom(
-        dir_dicom=pth_dicom,
-        pth_structures_file=pth_structure
-        )
+    phantom_obj = BrachyPhantom(dir_dicom=pth_dicom, pth_structures_file=pth_structure)
     phantom_obj.write_structures_to_nrrd(pth_out, True)
+
 
 def test_write_structures_to_dicom():
     pth_dicom = "../data_test/prostate-glen-p1-dcm"
-    pth_structure = glob(pth_dicom+"/RS*.dcm")[0]
+    pth_structure = glob(pth_dicom + "/RS*.dcm")[0]
     pth_out = "../data_test/test_export_plan/test_p1_dcm"
-    phantom_obj = BrachyPhantom(
-        dir_dicom=pth_dicom,
-        pth_structures_file=pth_structure
-        )
+    phantom_obj = BrachyPhantom(dir_dicom=pth_dicom, pth_structures_file=pth_structure)
     phantom_obj.write_image_to_dicom(pth_out)
     phantom_obj.write_structures_to_dicom(pth_out)
+
 
 def test_read_structures_from_nrrd():
     pth_dicom = "../data_test/prostate-glen-p1-dcm"
     pth_structures = "../data_test/prostate_glen_p1_structs.seg.nrrd"
     pth_out = "../data_test/test_export_plan/test_p1_dcm/rs.seg.nrrd"
-    phantom_obj = BrachyPhantom(
-        dir_dicom=pth_dicom,
-        pth_structures_file=pth_structures
-        )
+    phantom_obj = BrachyPhantom(dir_dicom=pth_dicom, pth_structures_file=pth_structures)
     # phantom_obj.write_image_to_nrrd(pth_out)
     phantom_obj.write_structures_to_nrrd(pth_out)
-    
+
+
 def test_write_to_egsphant():
     pth_dicom = "../data_test/prostate-glen-p1-dcm"
-    pth_structure = glob(pth_dicom+"/RS*.dcm")[0]
+    pth_structure = glob(pth_dicom + "/RS*.dcm")[0]
     pth_out = "../data_test/test_export_plan/test_ct.egsphant"
     # pth_materials = "../data_test/prostate-glen-p1-dcm/CTtoDensityProstate.txt"
     # assign_material_from_ct = True
@@ -94,8 +92,9 @@ def test_write_to_egsphant():
     phantom_obj.write_to_egsphant(
         pth_output=pth_out,
         material_dict=pth_materials,
-        assign_material_from_ct=assign_material_from_ct
+        assign_material_from_ct=assign_material_from_ct,
     )
+
 
 def test_load_egsphant():
     pth_egsphant = "../data_test/prostate-glen-p1-planFiles/ct.egsphant"
@@ -106,26 +105,27 @@ def test_load_egsphant():
         pth_output=pth_out,
     )
 
+
 def test_crop_phantom():
     pth_dicom = "../data_test/prostate-glen-p1-dcm"
-    pth_structure = glob(pth_dicom+"/RS*.dcm")[0]
+    pth_structure = glob(pth_dicom + "/RS*.dcm")[0]
     pth_out = "../data_test/test_export_plan"
-    phantom_obj = BrachyPhantom(
-        dir_dicom=pth_dicom,
-        pth_structures_file=pth_structure
-        )
+    phantom_obj = BrachyPhantom(dir_dicom=pth_dicom, pth_structures_file=pth_structure)
     crop_coordinates = np.array([[-100, 100], [-150, 200], [-1280, -1140]])
     phantom_obj.crop_by_coordinates(crop_coordinates)
-    phantom_obj.write_image_to_nrrd(pth_out+"/test_ct.nrrd")
-    phantom_obj.write_structures_to_nrrd(pth_out+"/test_rs.seg.nrrd")
+    phantom_obj.write_image_to_nrrd(pth_out + "/test_ct.nrrd")
+    phantom_obj.write_structures_to_nrrd(pth_out + "/test_rs.seg.nrrd")
+
 
 def test_catheter_table():
     from brachyutils.geometry_utils import CatheterTable
+
     pth_dicom = "../data_test/prostate-glen-p1-dcm"
-    pth_plan = glob(pth_dicom+"/RP*.dcm")[0]
+    pth_plan = glob(pth_dicom + "/RP*.dcm")[0]
 
     catheter_table = CatheterTable(pth_catheter_table=pth_plan)
     catheter_table.info()
+
 
 if __name__ == "__main__":
     print("testing BrachyPhantom")
