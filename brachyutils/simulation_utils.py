89c9768f1380e128915d0c701005298d4cdc799a
hosseinjafar <hosseinj.2555@gmail.com>
2024-11-02 18:54:48 -0400

    updated the constructors for the BrachSimulation and BrachySource. now loading from dict or just providing the variables in the constructor is sensible

--- brachyutils/simulation_utils.py
+++ brachyutils/simulation_utils.py
@@ -51,7 +51,17 @@ class BrachySource:
             and (reference_air_kerma is not None)
         ) != (
             source_dict is not None
-        ), "Either provide treatment_type, source_geometry, core_material, mass_number, atomic_number, air_kerma_per_history, reference_air_kerma or provide source_dict. Not both."
+        ), "Either provide treatment_type, source_geometry, core_material, mass_number,\
+            atomic_number, air_kerma_per_history, reference_air_kerma or provide source_dict. Not both."
+        
+        if source_dict is not None:
+            treatment_type = source_dict.get("treatment_type", None)
+            source_geometry = source_dict.get("source_geometry", None)
+            core_material = source_dict.get("core_material", None)
+            mass_number = source_dict.get("mass_number", None)
+            atomic_number = source_dict.get("atomic_number", None)
+            air_kerma_per_history = source_dict.get("air_kerma_per_history", None)
+            reference_air_kerma = source_dict.get("reference_air_kerma", None)
         
         self.treatment_type: str = treatment_type 
         self.source_geometry: str = source_geometry
@@ -60,6 +70,8 @@ class BrachySource:
         self.atomic_number: int = atomic_number 
         self.air_kerma_per_history: float = air_kerma_per_history 
         self.reference_air_kerma: float = reference_air_kerma 
+        
+        self.validate()
 
     def validate(self, verbose = False):
         r"""
@@ -100,7 +112,6 @@ class BrachySource:
         Dependencies:
             - None
         """
-        self.validate()
         return {
             "treatment_type": self.treatment_type,
             "source_geometry": self.source_geometry,
@@ -139,73 +150,92 @@ class BrachySimulation:
         RapidBrachyMC software.
     """
 
-    def __init__(self, simulation_dict: dict = None):
-        self.world_material: str = "Air"
-        self.number_histories: int = 2e9
-        self.total_time: float = None
-        self.dose_format: str = "nrrd"
-        self.number_of_threads: int = 16
-        self.control_verbose: int = 0
-        self.run_verbose: int = 0
-        self.tracking_verbose: int = 0
-        self.print_progress: int = int(self.number_histories / 100)
-
+    def __init__(
+        self,
+        brachy_source: BrachySource,
+        world_material: str = "Air",
+        number_histories: int = 2e9,
+        total_time: float = None,
+        dose_format: str = "nrrd",
+        number_of_threads: int = 16,
+        control_verbose: int = 0,
+        run_verbose: int = 0,
+        tracking_verbose: int = 0,
+        print_progress: int = 1e6,
+        simulation_dict: dict = None
+        ) -> None:
+        r"""
+        Purpose:
+            - A class to hold the information of a brachytherapy simulation.
+        Inputs:
+            - brachy_source: BrachySource
+            - world_material: str
+            - number_histories: int
+            - total_time: float
+            - dose_format: str
+            - number_of_threads: int
+            - control_verbose: int
+            - run_verbose: int
+            - tracking_verbose: int
+            - print_progress: int
+            - simulation_dict: dict
+        Attributes:
+            - brachy_source: BrachySource
+            - world_material: str
+            - number_histories: int
+            - total_time: float
+            - dose_format: str
+            - number_of_threads: int
+            - control_verbose: int
+            - run_verbose: int
+            - tracking_verbose: int
+            - print_progress: int
+        Functions:
+            - validate(): checks if the fields are valid for export.
+            - to_string(): converts the object to a string.
+        """
+        
+        assert (
+            (brachy_source is not None)
+            and (world_material is not None)
+            and (number_histories is not None)
+            and (total_time is not None)
+            and (dose_format is not None)
+            and (number_of_threads is not None)
+            and (control_verbose is not None)
+            and (run_verbose is not None)
+            and (tracking_verbose is not None)
+            and (print_progress is not None)
+        ) != (
+            simulation_dict is not None
+        ), "Either provide , brachy_source, world_material, number_histories, total_time,\
+            dose_format, number_of_threads, control_verbose, run_verbose, tracking_verbose, and\
+            print_progress or provide source_dict. Not both."
+        
         if simulation_dict is not None:
-            self.world_material = (
-                simulation_dict["world_material"]
-                if "world_material" in simulation_dict
-                else self.world_material
-            )
-            self.pth_plan = (
-                simulation_dict["pth_plan"]
-                if "pth_plan" in simulation_dict
-                else self.pth_plan
-            )
-            self.pth_phantom = (
-                simulation_dict["pth_phantom"]
-                if "pth_phantom" in simulation_dict
-                else self.pth_phantom
-            )
-            self.number_histories = (
-                simulation_dict["number_histories"]
-                if "number_histories" in simulation_dict
-                else self.number_histories
-            )
-            self.total_time = (
-                simulation_dict["total_time"]
-                if "total_time" in simulation_dict
-                else self.total_time
-            )
-            self.dose_format = (
-                simulation_dict["dose_format"]
-                if "dose_format" in simulation_dict
-                else self.dose_format
-            )
-            self.number_of_threads = (
-                simulation_dict["number_of_threads"]
-                if "number_of_threads" in simulation_dict
-                else self.number_of_threads
-            )
-            self.control_verbose = (
-                simulation_dict["control_verbose"]
-                if "control_verbose" in simulation_dict
-                else self.control_verbose
-            )
-            self.run_verbose = (
-                simulation_dict["run_verbose"]
-                if "run_verbose" in simulation_dict
-                else self.run_verbose
-            )
-            self.tracking_verbose = (
-                simulation_dict["tracking_verbose"]
-                if "tracking_verbose" in simulation_dict
-                else self.tracking_verbose
-            )
-            self.print_progress = (
-                simulation_dict["print_progress"]
-                if "print_progress" in simulation_dict
-                else self.print_progress
-            )
+            brachy_source = BrachySource(simulation_dict.get("source_dict", None))
+            world_material = simulation_dict.get("world_material", None)
+            number_histories = simulation_dict.get("number_histories", None)
+            total_time = simulation_dict.get("total_time", None)
+            dose_format = simulation_dict.get("dose_format", None)
+            number_of_threads = simulation_dict.get("number_of_threads", None)
+            control_verbose = simulation_dict.get("control_verbose", None)
+            run_verbose = simulation_dict.get("run_verbose", None)
+            tracking_verbose = simulation_dict.get("tracking_verbose", None)
+            print_progress = simulation_dict.get("print_progress", None)
+
+        self.brachy_source: BrachySource = brachy_source
+        self.world_material: str = world_material
+        self.number_histories: int = number_histories
+        self.total_time: float = total_time
+        self.dose_format: str = dose_format
+        self.number_of_threads: int = number_of_threads
+        self.control_verbose: int = control_verbose
+        self.run_verbose: int = run_verbose
+        self.tracking_verbose: int = tracking_verbose
+        self.print_progress: int = print_progress
+
+        self.validate()
 
     def validate(self, verbose = False):
         r"""
@@ -270,3 +300,27 @@ class BrachySimulation:
 /run/printProgress {self.print_progress}
 /sim/beamOn {self.number_histories}
 """
+
+    def to_dict(self):
+        r"""
+        Purpose:
+            - to convert the object to a dictionary.
+        Input:
+            - self: BrachySimulation
+        Output:
+            - a dictionary containing the information of the simulation.
+        Dependencies:
+            - None
+        """
+        return {
+            "source_dict": self.brachy_source.to_dict(),
+            "world_material": self.world_material,
+            "number_histories": self.number_histories,
+            "total_time": self.total_time,
+            "dose_format": self.dose_format,
+            "number_of_threads": self.number_of_threads,
+            "control_verbose": self.control_verbose,
+            "run_verbose": self.run_verbose,
+            "tracking_verbose": self.tracking_verbose,
+            "print_progress": self.print_progress
+        }
