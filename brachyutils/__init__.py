8d10b759d382364d629d673d09068d54d0046cb9
hosseinjafar <hosseinj.2555@gmail.com>
2024-12-11 13:50:45 -0500

    fixed a syntac bug that prevented imports

--- brachyutils/__init__.py
+++ brachyutils/__init__.py
@@ -9,7 +9,10 @@ __all__ = [
 ]
 
 # trunk-ignore(ruff/F401)
-from brachyutils.dose_utils import BrachyDose, DoseComparison
+from brachyutils.dose_utils import BrachyDose
+
+# trunk-ignore(ruff/F401)
+from brachyutils.dose_comparison_utils import DoseComparison
 
 # trunk-ignore(ruff/F401)
 from brachyutils.egsphant_utils import BrachyEgsphant
