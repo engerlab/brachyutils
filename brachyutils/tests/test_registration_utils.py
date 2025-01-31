3d92c8cea816e59f5b5a7e7a31f5386eae9d676a
hosseinjafar <hosseinj.2555@gmail.com>
2025-01-31 18:05:42 -0500

    just commenting and uncommenting

--- brachyutils/tests/test_registration_utils.py
+++ brachyutils/tests/test_registration_utils.py
@@ -4,18 +4,18 @@ from brachyutils.geometry_utils import BrachyPhantom
 
 def test_register_opentps():
     # Abdominal: static = CT, moving = MR
-    # pth_img_static = Path("../data_test/registration/abdomin_mr_ct/tr_ct_image_0001.nii.gz")
-    # pth_label_static = Path("../data_test/registration/abdomin_mr_ct/tr_ct_label_0001.nii.gz")
-    # pth_img_moving = Path("../data_test/registration/abdomin_mr_ct/tr_mr_image_0001.nii.gz")
-    # pth_label_moving = Path("../data_test/registration/abdomin_mr_ct/tr_mr_label_0001.nii.gz")
-    # pth_output = Path("../data_test/test_export_plan/abdomin_mr_ct/registered_abdomin_ct_mr.nrrd")
+    pth_img_static = Path("../data_test/registration/abdomin_mr_ct/tr_ct_image_0001.nii.gz")
+    pth_label_static = Path("../data_test/registration/abdomin_mr_ct/tr_ct_label_0001.nii.gz")
+    pth_img_moving = Path("../data_test/registration/abdomin_mr_ct/tr_mr_image_0001.nii.gz")
+    pth_label_moving = Path("../data_test/registration/abdomin_mr_ct/tr_mr_label_0001.nii.gz")
+    pth_output = Path("../data_test/test_export_plan/abdomin_mr_ct/registered_abdomin_ct_mr.nrrd")
 
-    # prostate: static = US, moving = MR
-    pth_img_static = Path("../data_test/registration/prostate_mr_us/train_us_image_case000000.nii.gz")
-    pth_img_moving = Path("../data_test/registration/prostate_mr_us/train_mr_image_case000000.nii.gz")
-    pth_label_static = Path("../data_test/registration/prostate_mr_us/train_us_label_case000000.nii.gz")
-    pth_label_moving = Path("../data_test/registration/prostate_mr_us/train_mr_label_case000000.nii.gz")
-    pth_output = Path("../data_test/test_export_plan/prostate/registered_phantom_us_mr.nrrd")
+    # # prostate: static = US, moving = MR
+    # pth_img_static = Path("../data_test/registration/prostate_mr_us/train_us_image_case000000.nii.gz")
+    # pth_img_moving = Path("../data_test/registration/prostate_mr_us/train_mr_image_case000000.nii.gz")
+    # pth_label_static = Path("../data_test/registration/prostate_mr_us/train_us_label_case000000.nii.gz")
+    # pth_label_moving = Path("../data_test/registration/prostate_mr_us/train_mr_label_case000000.nii.gz")
+    # pth_output = Path("../data_test/test_export_plan/prostate/registered_phantom_us_mr.nrrd")
 
     # prostate: static = MR, moving = US 
     # pth_img_static = Path("../data_test/registration/prostate_mr_us/train_mr_image_case000000.nii.gz")    
@@ -28,11 +28,11 @@ def test_register_opentps():
 
     static_phantom = BrachyPhantom(
         pth_phantom_file=pth_img_static,
-        # pth_structures_file=pth_label_static,
+        pth_structures_file=pth_label_static,
         )
     moving_phantom = BrachyPhantom(
         pth_phantom_file=pth_img_moving,
-        # pth_structures_file=pth_label_moving
+        pth_structures_file=pth_label_moving
         )
 
     static_phantom.export_to(
