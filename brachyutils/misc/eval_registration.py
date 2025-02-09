ec747670d749772833cfe82a1fa76b3209d116da
hosseinjafar <hosseinj.2555@gmail.com>
2025-02-08 21:23:48 -0500

    multi-processing conversion of the mr and ct abdomen files is implemented

--- brachyutils/misc/eval_registration.py
+++ brachyutils/misc/eval_registration.py
@@ -9,33 +9,33 @@ import pandas as pd
 from brachyutils.registration_utils import PhantomRegistration
 from brachyutils.geometry_utils import BrachyPhantom
 
-def export_phantom_opentps_nrrd_dicom_egsphant():
-    from brachyutils.geometry_utils import BrachyPhantom
-    pth_img_dicom = Path("../data_test/prostate-glen-p1-dcm")
-    pth_strct_dicom = glob(str(pth_img_dicom)+"/RS*.dcm")[0]
-    pth_img_nrrd = Path("../data_test/test_export_plan/opentps/prostate_glen_p1.nrrd")
-    pth_strct_nrrd = Path("../data_test/test_export_plan/opentps/prostate_glen_p1.seg.nrrd")
-    assign_material_from_ct = True
-    pth_materials = Path("../data_test/prostate-glen-p1-dcm/CTtoDensityProstate.txt")
-    phantom = BrachyPhantom(
-        dir_dicom=pth_img_dicom,
-        pth_structures_file=pth_strct_dicom
-    )
-    # phantom.export_to(
-    #     dir_nrrd_out=pth_img_nrrd.parent
-    # )
-    # phantom.export_to(
-    #     dir_dicom_out=Path.joinpath(pth_img_nrrd.parent, "dicom/")
-    # )
-    phantom.write_to_egsphant(
-        pth_output=pth_img_nrrd.parent.joinpath("egsphant.seq.nrrd"),
-        material_dict=pth_materials,
-        assign_material_from_ct=assign_material_from_ct
-        )
+# def export_phantom_opentps_nrrd_dicom_egsphant():
+#     from brachyutils.geometry_utils import BrachyPhantom
+#     pth_img_dicom = Path("../data_test/prostate-glen-p1-dcm")
+#     pth_strct_dicom = glob(str(pth_img_dicom)+"/RS*.dcm")[0]
+#     pth_img_nrrd = Path("../data_test/test_export_plan/opentps/prostate_glen_p1.nrrd")
+#     pth_strct_nrrd = Path("../data_test/test_export_plan/opentps/prostate_glen_p1.seg.nrrd")
+#     assign_material_from_ct = True
+#     pth_materials = Path("../data_test/prostate-glen-p1-dcm/CTtoDensityProstate.txt")
+#     phantom = BrachyPhantom(
+#         dir_dicom=pth_img_dicom,
+#         pth_structures_file=pth_strct_dicom
+#     )
+#     # phantom.export_to(
+#     #     dir_nrrd_out=pth_img_nrrd.parent
+#     # )
+#     # phantom.export_to(
+#     #     dir_dicom_out=Path.joinpath(pth_img_nrrd.parent, "dicom/")
+#     # )
+#     phantom.write_to_egsphant(
+#         pth_output=pth_img_nrrd.parent.joinpath("egsphant.seq.nrrd"),
+#         material_dict=pth_materials,
+#         assign_material_from_ct=assign_material_from_ct
+#         )
 
-def compare_dose_mc_tg43():
-    from brachyutils.dose_generation_utils import DoseMonteCarlo, DoseTG43
-    from brachyutils.plan_utils import BrachyPlan
+# def compare_dose_mc_tg43():
+#     from brachyutils.dose_generation_utils import DoseMonteCarlo, DoseTG43
+#     from brachyutils.plan_utils import BrachyPlan
 
 def evaluate_contourBased_registration(
     dir_static: str | Path,
@@ -197,7 +197,7 @@ def run_registeration():
         multi_thread=True
     )
 
-def organize_data(dir_out: str | Path):
+def organize_data(dir_out: str | Path, multi_thread: bool = False):
     r"""
     Purpose:
         - to gather data from all formats and directories into one static directory,
@@ -236,26 +236,54 @@ def organize_data(dir_out: str | Path):
             "moving_img": pth_moving_img[0],
             "moving_seg": pth_moving_seg[0]
         })
-        
+
     dir_out = Path(dir_out)
     dir_out.mkdir(parents=True, exist_ok=True)
     dir_static = dir_out.joinpath("static")
     dir_moving = dir_out.joinpath("moving")
-    dir_registered = dir_out.joinpath("reg")    
-    for case in all_cases:
-        static_phantom = BrachyPhantom(
-            pth_phantom_file=case.get("static_img"),
-            pth_structures_file=case.get("static_seg")
-        )
-        moving_phantom = BrachyPhantom(
-            pth_phantom_file=case.get("moving_img"),
-            pth_structures_file=case.get("moving_seg")
-        )
-        static_phantom.export_to(dir_nrrd_out=dir_static)
-        moving_phantom.export_to(dir_nrrd_out=dir_moving)
-        return
-    
+    dir_registered = dir_out.joinpath("reg")   
+    if multi_thread:
+        import asyncio
+        from concurrent.futures import ThreadPoolExecutor
+        async def run_in_executor(executor, case):
+            loop = asyncio.get_event_loop()
+            try:
+                return await loop.run_in_executor(executor, export_static_moving_phantoms, case, dir_static, dir_moving)
+            except Exception as e:
+                print(f"error in exporting {case}")
+                print(e)
+                return None
+
+        async def main():
+            with ThreadPoolExecutor() as executor:
+                tasks = []
+                for case in all_cases:
+                    tasks.append(run_in_executor(executor, case))
+                await asyncio.gather(*tasks)
+
+        asyncio.run(main())
+    else: 
+        for case in all_cases:
+            try:
+                export_static_moving_phantoms(case, dir_static, dir_moving)
+                return
+            except Exception as e:
+                print(f"error in exporting {case}")
+                print(e)
+
+def export_static_moving_phantoms(case: Dict, dir_static: Path, dir_moving: Path):
+    static_phantom = BrachyPhantom(
+        pth_phantom_file=case.get("static_img"),
+        pth_structures_file=case.get("static_seg")
+    )
+    moving_phantom = BrachyPhantom(
+        pth_phantom_file=case.get("moving_img"),
+        pth_structures_file=case.get("moving_seg")
+    )
+    static_phantom.export_to(dir_nrrd_out=dir_static)
+    moving_phantom.export_to(dir_nrrd_out=dir_moving)
+
 if __name__ == "__main__": 
     # export_phantom_opentps_nrrd_dicom_egsphant()
     # run_registeration()
-    organize_data("../temp_data/registration/abdomen-mr-ct")
+    organize_data("../temp_data/registration/abdomen-mr-ct", True)
