89b18f8fd98d2d5775aeefe506efd946040c26bd
hosseinjafar <hosseinj.2555@gmail.com>
2025-02-17 18:26:36 -0500

    Added a few lines to start each service independently

--- docker_src/command_docker.sh
+++ docker_src/command_docker.sh
@@ -2,6 +2,9 @@
 # to build the image and run the container
 # docker compose up --build -d BrachyUtils
 # to run the container without building the image
-docker compose up --no-build -d BrachyUtils
+docker compose up --no-build -d BrachyUtils 
+# docker compose up --no-build -d DoseCalcMC
+# docker compose up --no-build -d DoseCalcTG43
+
 # to enter the container
-docker exec -it BrachyUtils bash
+# docker exec -it BrachyUtils bash
