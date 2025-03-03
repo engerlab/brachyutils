a44f9ebfdccc29f2c9dd09c6216b79144d598fa4
hosseinjafar <hosseinj.2555@gmail.com>
2025-03-03 17:43:31 -0500

    in the process of debugging the warp with plastimatch. i finally decided that the client would provide the full path and the server has to decided where to put it in the temp_data folder.

--- docker_src/command_docker.sh
+++ docker_src/command_docker.sh
@@ -2,7 +2,7 @@
 # to build the image and run the container
 # docker compose up --build -d BrachyUtils
 # to run the container without building the image
-docker compose up --no-build -d BrachyUtils
+docker compose up --no-build -d BrachyUtils 
 # docker compose up --no-build -d DoseCalcMC
 # docker compose up --no-build -d DoseCalcTG43
 docker compose up --no-build -d Pyplastimatch
