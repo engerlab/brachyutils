1100241fce4197ab305d93cddb42f5ace5365351
hosseinjafar <hosseinj.2555@gmail.com>
2025-03-05 17:46:54 -0500

    exporting the dvh metrics of the patients 1-6

--- brachyutils/misc/eval_dose_generation.py
+++ brachyutils/misc/eval_dose_generation.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from brachyutils.plan_utils import BrachyPlan, load_dicom_to_plan
 from brachyutils.dose_comparison_utils import DoseComparison
 from brachyutils.dose_generation_utils import DoseGenerator, DoseMonteCarlo, DoseTG43
+from pandas import DataFrame
 
 def run_multi_proc(function, input_list):
     import asyncio
@@ -81,6 +82,56 @@ def export_single_dicom_to_plan(
     )
     return dir_export_plan
 
+def run_export():
+    from functools import partial
+    pth_single_dicom = Path("/root/YourLocalHome/Data/prostate/prostate-glen-2023")
+    dir_export = Path("../temp_data/mc/prostate-glen-2023")
+    # pth_material = Path("../admin/constants/CTtoDensityProstate.txt")
+    # mat_from_ct = True
+    pth_material = Path("../admin/constants/structure_materials_prostate.json")
+    mat_from_ct = False
+    crop_by_contour = "body"
+    sim_dict = {
+        "source_dict": {
+            "treatment_type": "HDR",
+            "source_geometry": "MicroSelectronV2",
+            "core_material": "G4_Ir",
+            "mass_number": "192",
+            "atomic_number": "77",
+            "air_kerma_per_history": 1.149000e-11,
+            "reference_air_kerma": 5e04,
+        },
+        "pth_plan": "combined.plan",
+        "pth_phantom": "ct.egsphant",
+        "number_histories": 1000000,
+        "total_time": 0,
+        "number_of_threads": 32,
+        "PrintProgress": 10000,
+        "beam_on": 10000,
+    }
+    content_to_export = {
+        "egsphant": True,
+        "materials_table": pth_material,
+        "assign_material_from_ct": mat_from_ct,
+        "resample_egsphant_to": [1., 1., 1.],
+        "crop_by_contour": crop_by_contour,
+        "plan": True,
+        "mac": True,
+        "ApplicatorMaterials": False,
+        "applicator_geometry": False,
+    }
+
+    all_dicoms = list(pth_single_dicom.glob("*/"))
+    all_inputs = []
+    partially_filled_export_func = partial(
+        export_single_dicom_to_plan,
+        dir_export=dir_export,
+        sim_dict=sim_dict,
+        content_to_export=content_to_export,
+        )
+
+    run_multi_proc(partially_filled_export_func, all_dicoms)
+
 def get_dvh_metrics_single_plan(
     dir_dicom: Path | str,
     dvh_metric_goals: Dict[str, float],
@@ -119,7 +170,53 @@ def get_dvh_metrics_single_plan(
         )
     return dvh_metrics_observed
 
-# def get_dvh_metrics_all_plans()
+def get_dvh_metrics_all_plans(
+    dir_all_dicom_folders: str | Path,
+    dir_all_plan_folders: str | Path,
+    dvh_metric_goals: Dict[str, float],
+):
+    r"""
+    ### Purpose:
+        - To get the dvh metrics from all the patients in the given directory.
+    
+    ### Inputs:
+        - dir_dicom_folders: str | Path: The path to the dicom folders.
+        - dir_plan_folders: str | Path: The path to the plan folders.
+        - dvh_metric_goals: Dict[str, float]: The DVH metrics to evaluate the plan.
+    """
+    dir_all_dicom_folders = Path(dir_all_dicom_folders)
+    dir_all_plan_folders = Path(dir_all_plan_folders)
+    
+    list_dir_plan = dir_all_plan_folders.glob("*/")
+    all_dvhs = []
+    for dir_plan in list_dir_plan:
+        if not dir_plan.is_dir():
+            continue
+        dir_dicom = dir_all_dicom_folders.joinpath(dir_plan.stem)
+        print(f"dvh from dicom folder: {dir_dicom}")
+        dvh_metrics = get_dvh_metrics_single_plan(
+            dir_dicom=dir_dicom,
+            dvh_metric_goals=dvh_metric_goals,
+            dir_plan_export=dir_plan,
+        )
+        all_dvhs.append({dir_plan.stem: dvh_metrics})
+
+    df_dvhs = DataFrame(all_dvhs)
+    df_dvhs.to_csv(dir_all_plan_folders/"dvh_metrics.csv")
+
+def run_get_dvh_metrics_all_plans():
+    pth_all_dicom = Path("/root/YourLocalHome/Data/prostate/prostate-glen-2023")
+    dir_all_plans = Path("/root/YourLocalHome/Data/prostate/plans-1mm/prostate-glen-2023")
+    dvh_metric_goals = {
+        "D95%(ctv)": 15,
+        "D1cc(rectum)": 11.25,
+        "D0.1cc(urethra)": 18.75,
+    }
+    get_dvh_metrics_all_plans(
+        dir_all_dicom_folders=pth_all_dicom,
+        dir_all_plan_folders=dir_all_plans,
+        dvh_metric_goals=dvh_metric_goals,
+    )
 
 def test_get_dvh_metrics_single_plan():
     pth_single_dicom = Path("/root/YourLocalHome/Data/prostate/prostate-glen-2023/p1")
@@ -183,58 +280,9 @@ def test_dose_calc():
     dose_generator_class = DoseMonteCarlo
     generate_single_dose(dir_plan_export, dose_generator_class)
 
-def run_export():
-    from functools import partial
-    pth_single_dicom = Path("/root/YourLocalHome/Data/prostate/prostate-glen-2023")
-    dir_export = Path("../temp_data/mc/prostate-glen-2023")
-    # pth_material = Path("../admin/constants/CTtoDensityProstate.txt")
-    # mat_from_ct = True
-    pth_material = Path("../admin/constants/structure_materials_prostate.json")
-    mat_from_ct = False
-    crop_by_contour = "body"
-    sim_dict = {
-        "source_dict": {
-            "treatment_type": "HDR",
-            "source_geometry": "MicroSelectronV2",
-            "core_material": "G4_Ir",
-            "mass_number": "192",
-            "atomic_number": "77",
-            "air_kerma_per_history": 1.149000e-11,
-            "reference_air_kerma": 5e04,
-        },
-        "pth_plan": "combined.plan",
-        "pth_phantom": "ct.egsphant",
-        "number_histories": 1000000,
-        "total_time": 0,
-        "number_of_threads": 32,
-        "PrintProgress": 10000,
-        "beam_on": 10000,
-    }
-    content_to_export = {
-        "egsphant": True,
-        "materials_table": pth_material,
-        "assign_material_from_ct": mat_from_ct,
-        "resample_egsphant_to": [1., 1., 1.],
-        "crop_by_contour": crop_by_contour,
-        "plan": True,
-        "mac": True,
-        "ApplicatorMaterials": False,
-        "applicator_geometry": False,
-    }
-
-    all_dicoms = list(pth_single_dicom.glob("*/"))
-    all_inputs = []
-    partially_filled_export_func = partial(
-        export_single_dicom_to_plan,
-        dir_export=dir_export,
-        sim_dict=sim_dict,
-        content_to_export=content_to_export,
-        )
-
-    run_multi_proc(partially_filled_export_func, all_dicoms)
-
 if __name__ == "__main__":
     # test_export()
-    # run_export()
     # test_dose_calc()
-    test_get_dvh_metrics_single_plan()
+    # test_get_dvh_metrics_single_plan()
+    # run_export()
+    run_get_dvh_metrics_all_plans()
