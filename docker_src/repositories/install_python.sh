73bd3ae7cdf0c3abba76d6c9fca19a4b6ce4a8dc
hosseinjafar <hosseinj.2555@gmail.com>
2025-04-10 16:24:16 -0400

    in progress of making the docker image with brachyutils. the packaing does not work as it is

--- docker_src/repositories/install_python.sh
+++ docker_src/repositories/install_python.sh
@@ -4,7 +4,9 @@ num_threads=10
 
 apt install -y build-essential zlib1g zlib1g-dev libncurses5-dev \
     libgdbm-dev pkg-config libnss3-dev libssl-dev libreadline-dev \
-    libffi-dev libsqlite3-dev wget nano liblzma-dev libbz2-dev
+    libffi-dev libsqlite3-dev wget nano liblzma-dev libbz2-dev \
+    libxrender1 libgl1 libglib2.0-0 python3-tk
+
 # install python 3.13
 cd ${dir_software} || exit
 wget -nc https://www.python.org/ftp/python/3.13.1/Python-3.13.1.tgz
