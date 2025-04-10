a6009c040a42d9f278e009fe5f667b66f6b0b93e
hosseinjafar <hosseinj.2555@gmail.com>
2025-04-10 10:23:27 -0400

    renamed the installation scripts

---
+++ install_python.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+dir_software=${HOME}/Software
+num_threads=10
+
+apt install -y build-essential zlib1g zlib1g-dev libncurses5-dev \
+    libgdbm-dev pkg-config libnss3-dev libssl-dev libreadline-dev \
+    libffi-dev libsqlite3-dev wget nano liblzma-dev
+# install python 3.13
+cd ${dir_software} || exit
+wget -nc https://www.python.org/ftp/python/3.13.1/Python-3.13.1.tgz
+tar -xf Python-3.13.1.tgz
+cd Python-3.13.1 || exit
+./configure --enable-optimizations
+make -j${num_threads}
+make altinstall
+echo alias python=python3.13 >>~/.bash_aliases
+echo alias pip=pip3.13 >>~/.bash_aliases
+source ${HOME}/.bashrc
