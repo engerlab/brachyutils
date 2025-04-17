bc75cee0e4d23e689856ee263b1a462a411c6845
hosseinjafar <hosseinj.2555@gmail.com>
2025-04-17 15:50:38 -0400

    nothing important

--- brachyutils/tests/test_geometry_utils.py
+++ brachyutils/tests/test_geometry_utils.py
@@ -174,12 +174,13 @@ def test_catheter_table():
 def test_catheter():
     from brachyutils.geometry.catheter_utils import Catheter, DwellPosition
     # # create a catheter from tip and last dwell position
-    # new_catheter = Catheter(
-        # index=0,
-        # tip_position=[25, 25, 25],
-        # last_dwell_coordinate=[0, 0, 0]
-    # )
-    # # create a catheter from digitization points
+    new_catheter = Catheter(
+        index=0,
+        tip_position=[25, 25, 25],
+        last_dwell_coordinate=[0, 0, 0]
+    )
+    print(new_catheter)
+    # create a catheter from digitization points
     coordinates_on_1_axis = np.arange(53, 1.5, -2)
     points = np.stack([
         coordinates_on_1_axis,
