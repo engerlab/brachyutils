45b20727b9e1f960c78244d395db291e5ab5b72a
hosseinjafar <hosseinj.2555@gmail.com>
2025-07-12 12:55:09 -0400

    having files in the root folder is a bad idea since it does not allow the host machine to make changes to the files made by the container. I am moving everything to /app instead of /root. this requires the rebuild of the docker image for BrachyUtils and the rest

--- docker_src/command_docker.sh
+++ docker_src/command_docker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 xhost +local: # to allow the container to access the host display
+# set the environment variables for the container
+export UID=$(id -u)
+export GID=$(id -g)
+export DISPLAY=:0
 # to build the image and run the container
 # docker compose up --build -d BrachyUtils
 # to run the container without building the image
