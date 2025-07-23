cf86db5046b39f8ef04a49701a866fb098723a7c
hosseinjafar <hosseinj.2555@gmail.com>
2025-07-23 14:27:31 -0400

    converting phantom files worked very well both sequentially and in parallel

--- brachyutils/tests/test_cli_utils.py
+++ brachyutils/tests/test_cli_utils.py
@@ -16,14 +16,20 @@ def test_convert_dose():
 
 def test_convert_phantom():
     from brachyutils.cli_utils import convert_phantom, PhantomType
-    dir_input = Path("data_test/prostate-glen-p1-dcm")
+    # # for a single file
+    # dir_input = Path("data_test/prostate-glen-p1-dcm")
+    # dir_output = Path("data_test/test_export_plan/prostate")
+
+    # # for multiple files
+    dir_input = Path("/home/ubuntu/YourLocalHome/Data/prostate/prostate-glen-2023")
     dir_output = Path("data_test/test_export_plan/prostate")
+
     type_out = PhantomType.NRRD
     convert_phantom(
-        pth_inputs=[dir_input],
+        pth_inputs=list(dir_input.glob("*/")),
         type_out=type_out,
         dir_output=dir_output,
-        multi_proc=False
+        multi_proc=True
     )
 
 if __name__ == "__main__":
