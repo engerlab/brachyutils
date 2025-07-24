ecff78fdf3770f68d4a78330668612471067b9c5
sebquetin <sebastienquetin@hotmail.fr>
2025-07-24 11:28:08 -0400

    updating CT to denstiy egsphant creation based on HU limit being the UPPER limit and not the LOWER limit. See 'DOSXYZnrc Users Manual' by Walters et al.

--- brachyutils/geometry/egsphant_utils.py
+++ brachyutils/geometry/egsphant_utils.py
@@ -25,7 +25,7 @@ class BrachyEgsphant:
         - material_dict: dict := a dictionary containing the name of the elements for each voxel,
         and the following keys: [
             "density" := the density of the material in g/cm^3,
-            "HU_limit" := the lower HU limit threshold of the material,
+            "HU_limit" := the upper HU limit threshold of the material,
             "structure_name := {optional} the name of the structure in the dicom file that represents the material,"
             ]
         - unit_length: str := the unit of the length of the axis is mm.
@@ -78,7 +78,7 @@ class BrachyEgsphant:
             - material_dict: dict := a dictionary containing the name of the elements for each voxel,
                 and the following keys: [
                     "density" := the density of the material in g/cm^3,
-                    "HU_limit" := the lower HU limit threshold of the material,
+                    "HU_limit" := the upper HU limit threshold of the material,
                     "structure_name := {optional} the name of the structure in the dicom file that represents the material,"
                 ]
             - assign_material_from_ct:bool := if True, the function will assign the material based on the HU values in the CT image.
@@ -815,7 +815,7 @@ class BrachyEgsphant:
             the following keys are required for each material:
                 - encoding: a single character string that represents the material in the material matrix
                 - density: the density of the material in g/cm^3
-                - HU_limit: the lower HU limit of the material
+                - HU_limit: the upper HU limit of the material
                 - structure_name: the name of the structure in the dicom file that represents the material [optional]
                 - structure_size: the size of the structure in the dicom file that represents the material [optional]
         Outputs:
@@ -861,60 +861,38 @@ class BrachyEgsphant:
             # sort out the materials and density based on the HU values
             # sort out the materials and density based on the HU values
             self._sort_materials_by("HU_limit")
-            low_HU_threshold = np.min(
-                [
-                    phantom_ct_image.min(),
-                    self.material_dict.get(background_material).get("HU_limit"),
-                ]
-            )  # -np.inf #self.material_dict.get(background_material).get("HU_limit")
-            density_low_bound = background_density
+            low_HU_threshold = - float("inf")
 
             for i, material in enumerate(list(self.material_dict.keys())):
 
                 # numerically interpolate the density and material based on the HU values
-                low_HU_threshold = self.material_dict.get(material).get("HU_limit")
-                # if this is the last material, set the high HU threshold to infinity
-                high_HU_threshold = (
-                    self.material_dict.get(list(self.material_dict.keys())[i + 1]).get(
-                        "HU_limit"
-                    )
-                    if i + 1 < len(self.material_dict)
-                    else np.inf
-                )
+                high_HU_threshold = self.material_dict.get(material).get("HU_limit")
+
                 # find region of interest mask based on the HU values
                 roi_mask = np.logical_and(
                     phantom_ct_image > low_HU_threshold,
                     phantom_ct_image <= high_HU_threshold,
                 )
 
-                density_low_bound = self.material_dict.get(material).get("density")
-                density_high_bound = (
-                    self.material_dict.get(list(self.material_dict.keys())[i + 1]).get(
-                        "density"
-                    )
-                    if i + 1 < len(self.material_dict)
-                    else density_low_bound
-                )
-
-                slope_density_over_HU = (density_high_bound - density_low_bound) / (
-                    high_HU_threshold - low_HU_threshold
-                )
                 # interpolate density based on the HU value
-                # density_matrix *= np.logical_not(roi_mask)
                 if material == background_material:
                     density_matrix = np.where(
                         roi_mask,
-                        density_low_bound,
+                        self.material_dict.get(material).get("density"),
                         density_matrix,
                     )
                 else:
+                    density_high_bound = self.material_dict.get(material).get("density")
+                    slope_density_over_HU = (density_high_bound - density_low_bound) / (
+                        high_HU_threshold - low_HU_threshold
+                    )
+                    # interpolate density based on the HU value
                     density_matrix = np.where(
                         roi_mask,
                         ((phantom_ct_image - low_HU_threshold) * slope_density_over_HU)
                         + density_low_bound,
                         density_matrix,
                     )
-                # material_matrix *= np.logical_not(roi_mask)
                 material_matrix = np.where(
                     roi_mask,
                     BrachyEgsphant._materials_encoding_array.index(
@@ -922,6 +900,17 @@ class BrachyEgsphant:
                     ),
                     material_matrix,
                 )
+                low_HU_threshold = self.material_dict.get(material).get("HU_limit")
+                density_low_bound = self.material_dict.get(material).get("density")
+
+            last_mat = list(self.material_dict.keys())[-1]
+            low_HU_threshold = self.material_dict.get(last_mat).get("HU_limit")
+            roi_mask = phantom_ct_image > low_HU_threshold
+            material_matrix[roi_mask] = BrachyEgsphant._materials_encoding_array.index(
+                    self.material_dict.get(last_mat).get("encoding")
+                )
+            density_matrix[roi_mask] = self.material_dict.get(last_mat).get("density")
+
         else:
             #JK here
             #Sometimes we need to assign the same material to multiple contours
@@ -1128,12 +1117,12 @@ def _load_material_dict(material_source: Union[Path, dict]):
     r"""
     Purpose:
         To load material dictionary and give it the proper keys from simple material dictionary,
-        a ct to density.txt file or from a json file that contains the density and HU lower
+        a ct to density.txt file or from a json file that contains the density and HU upper
         limit threshold for each material.
     Inputs:
         - material_source := directory path to the ct2density.txt file, json file or the material dictionary
     Outputs:
-        - dict := a dictionary containing the density and HU lower limit thresholds for each material.
+        - dict := a dictionary containing the density and HU upper limit thresholds for each material.
     """
     if isinstance(material_source, Path) or isinstance(material_source, str):
         pth_file = material_source
