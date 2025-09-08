fixed the bug in reading the egsphant. note that the material image is of type int and it starts from 0, while the encodings start from 1. there is an off by one error in reading the density from an egsphant file

brachyutils/geometry/egsphant_utils.py
@@ -284,7 +284,7 @@ class BrachyEgsphant:
                 encoding = int(self.material_dict[material]["encoding"])
                 density = np.unique(
                     self.density_image.imageArray[
-                        self.material_image.imageArray == encoding
+                        self.material_image.imageArray == encoding-1
                     ]
                 )
                 density = density.min() if len(density) != 0 else 0