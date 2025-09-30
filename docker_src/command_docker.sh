08d92c66b7b230304b9c050c16c64812888f229c
hosseinjafar <hosseinj.2555@gmail.com>
2025-09-30 16:22:53 -0400

    i think i got the benchmarking for plastimatch down. now on to the debugging the benchmarking process for plastimatch

--- docker_src/command_docker.sh
+++ docker_src/command_docker.sh
@@ -15,6 +15,6 @@ docker compose up --no-build -d BrachyUtils
 # docker compose up --no-build -d DoseCalcMC
 # docker compose up --no-build -d DoseCalcTG43
 docker compose up --no-build -d Plastimatch
-docker compose up --no-build -d SimpleElastix
+# docker compose up --no-build -d SimpleElastix
 # to enter the container
 docker exec -it BrachyUtils bash
