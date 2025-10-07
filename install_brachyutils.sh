4773818df91062ca91a2fdf45dcee871cbf7fad5
hosseinjafar <hosseinj.2555@gmail.com>
2025-10-07 17:39:54 -0400

    finally, remade a new docker image

--- install_brachyutils.sh
+++ install_brachyutils.sh
@@ -9,7 +9,7 @@ apt install -y build-essential zlib1g zlib1g-dev libncurses5-dev \
     libgdbm-dev pkg-config libnss3-dev libssl-dev libreadline-dev \
     libffi-dev libsqlite3-dev wget nano liblzma-dev libbz2-dev \
     libxrender1 libgl1 libglib2.0-0 tk-dev xz-utils llvm
-
-git clone git@github.com:engerlab/OpenTPS-brachyutils.git ${dir_software}/brachyutils
+rm -rf ${dir_software}/brachyutils
+git clone git@github.com:engerlab/brachyutils.git ${dir_software}/brachyutils
 python3.13 -m pip install -e ${dir_software}/brachyutils
 python3.13 -m amplpy.modules install highs gurobi xpress cplex scip gcg
