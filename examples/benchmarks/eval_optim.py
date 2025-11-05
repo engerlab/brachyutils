eb4d325340e8f134a1266b378d57f32615077312
hosseinjjafar <hosseinj.2555@gmail.com>
2025-11-05 10:00:07 -0500

    removed commented out code

--- examples/benchmarks/eval_optim.py
+++ examples/benchmarks/eval_optim.py
@@ -38,44 +38,6 @@ def get_a_plan_to_optimize(
     if len(dose_rate_files) < 1 and not generate_dose_rates:
         raise FileNotFoundError(f"No dose rate files found in {dir_dose_rates}. Set generate_dose_rates=True to create them.")
 
-    # if dvh_metric_goals is None:
-    #     dvh_metric_goals = {
-    #         "D95%(CTV)": target_dose,
-    #         "D1cc(RECTUM)": target_dose * 0.75,
-    #         "D0.1cc(URETHRA)": target_dose * 1.25,
-    #         "CI(CTV)": 1.0,
-    #         "HI(CTV)": 0.5,
-    #     }
-    # if optimization_config_list is None:
-    #     optimization_config_list=[
-    #         Optimization_Config(
-    #             structure_name="CTV",
-    #             dose_voxel_goal=dvh_metric_goals["D95%(CTV)"],
-    #             penalty_weight_linear=300,
-    #             # penalty_weight_quadratic=1,
-    #             # penalty_weight_uniformity=0,
-    #             # penalty_weight_hotspot=1,
-    #             # hotspot_threshold=1.5,
-    #             mask_margin_mm=0,
-    #             spacing_mm=3),
-    #         Optimization_Config(
-    #             structure_name="URETHRA",
-    #             dose_voxel_goal=0,
-    #             penalty_weight_linear=1,
-    #             # penalty_weight_quadratic=1,
-    #             # penalty_weight_uniformity=0,
-    #             mask_margin_mm=0,
-    #             spacing_mm=1),
-    #         Optimization_Config(
-    #             structure_name="RECTUM",
-    #             dose_voxel_goal=0,
-    #             penalty_weight_linear=1,
-    #             # penalty_weight_quadratic=1,
-    #             # penalty_weight_uniformity=0,
-    #             mask_margin_mm=0,
-    #             spacing_mm=3)
-    #     ]
-
     plan_obj = load_dicom_to_plan(
         dir_dicom=pth_dicom,
         load_dicom_dose=False,
