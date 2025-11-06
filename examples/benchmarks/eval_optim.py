1b16d93543d55047f8d045e2767d54bf17ccc5b7
hosseinjjafar <hosseinj.2555@gmail.com>
2025-11-06 17:51:42 -0500

    finally getting ready for the big trial on gurobi

--- examples/benchmarks/eval_optim.py
+++ examples/benchmarks/eval_optim.py
@@ -180,6 +180,7 @@ def run_optimization(
         "status": status,
         **result_dvh_metrics
     }
+    del optim_obj
     return optim_trial_result
 
 def eval_optim(
@@ -324,6 +325,32 @@ def eval_optim(
                 mask_margin_mm=0,
                 spacing_mm=3)
         ],
+        "LQHU": [
+            Optimization_Config(
+                structure_name="CTV",
+                dose_voxel_goal=target_dose,
+                penalty_weight_linear=300,
+                penalty_weight_quadratic=1,
+                penalty_weight_uniformity=1,
+                penalty_weight_hotspot=1,
+                hotspot_threshold=1.5,
+                mask_margin_mm=0,
+                spacing_mm=3),
+            Optimization_Config(
+                structure_name="URETHRA",
+                dose_voxel_goal=0,
+                penalty_weight_linear=1,
+                penalty_weight_quadratic=1,
+                mask_margin_mm=0,
+                spacing_mm=1),
+            Optimization_Config(
+                structure_name="RECTUM",
+                dose_voxel_goal=0,
+                penalty_weight_linear=1,
+                penalty_weight_quadratic=1,
+                mask_margin_mm=0,
+                spacing_mm=3)
+        ],
     }
 
     for pth_dicom in dir_all_dicoms:
@@ -351,7 +378,7 @@ def eval_optim(
                         "objective_terms": config_var
                     }
                     results_solver.to_csv(
-                        dir_all_dose_rates/f"eval_optim_results.csv",
+                        dir_all_dose_rates/f"eval_optim_results_{package}.csv",
                         index=False)
 
 if __name__ == "__main__":
