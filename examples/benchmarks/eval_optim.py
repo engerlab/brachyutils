ab1b03c6a0cb157870a66d0a44e5d182ead9d91e
hosseinjjafar <hosseinj.2555@gmail.com>
2025-11-10 09:19:27 -0500

    toying with the upper and lower bound for the hot spot term showed that it works as expected.

--- examples/benchmarks/eval_optim.py
+++ examples/benchmarks/eval_optim.py
@@ -236,55 +236,55 @@ def eval_optim(
                 mask_margin_mm=0,
                 spacing_mm=3)
         ],
-        # "LQ": [
-        #     Optimization_Config(
-        #         structure_name="CTV",
-        #         dose_voxel_goal=target_dose,
-        #         penalty_weight_linear=300,
-        #         penalty_weight_quadratic=1,
-        #         mask_margin_mm=0,
-        #         spacing_mm=3),
-        #     Optimization_Config(
-        #         structure_name="URETHRA",
-        #         dose_voxel_goal=0,
-        #         penalty_weight_linear=1,
-        #         penalty_weight_quadratic=1,
-        #         mask_margin_mm=0,
-        #         spacing_mm=1),
-        #     Optimization_Config(
-        #         structure_name="RECTUM",
-        #         dose_voxel_goal=0,
-        #         penalty_weight_linear=1,
-        #         penalty_weight_quadratic=1,
-        #         mask_margin_mm=0,
-        #         spacing_mm=3)
-        # ],
-        # "LQU": [
-        #     Optimization_Config(
-        #         structure_name="CTV",
-        #         dose_voxel_goal=target_dose,
-        #         penalty_weight_linear=300,
-        #         penalty_weight_quadratic=1,
-        #         penalty_weight_uniformity=1,
-        #         mask_margin_mm=0,
-        #         spacing_mm=3),
-        #     Optimization_Config(
-        #         structure_name="URETHRA",
-        #         dose_voxel_goal=0,
-        #         penalty_weight_linear=1,
-        #         penalty_weight_quadratic=1,
-        #         penalty_weight_uniformity=1,
-        #         mask_margin_mm=0,
-        #         spacing_mm=1),
-        #     Optimization_Config(
-        #         structure_name="RECTUM",
-        #         dose_voxel_goal=0,
-        #         penalty_weight_linear=1,
-        #         penalty_weight_quadratic=1,
-        #         penalty_weight_uniformity=1,
-        #         mask_margin_mm=0,
-        #         spacing_mm=3)
-        # ],
+        "LQ": [
+            Optimization_Config(
+                structure_name="CTV",
+                dose_voxel_goal=target_dose,
+                penalty_weight_linear=300,
+                penalty_weight_quadratic=1,
+                mask_margin_mm=0,
+                spacing_mm=3),
+            Optimization_Config(
+                structure_name="URETHRA",
+                dose_voxel_goal=0,
+                penalty_weight_linear=1,
+                penalty_weight_quadratic=1,
+                mask_margin_mm=0,
+                spacing_mm=1),
+            Optimization_Config(
+                structure_name="RECTUM",
+                dose_voxel_goal=0,
+                penalty_weight_linear=1,
+                penalty_weight_quadratic=1,
+                mask_margin_mm=0,
+                spacing_mm=3)
+        ],
+        "LQU": [
+            Optimization_Config(
+                structure_name="CTV",
+                dose_voxel_goal=target_dose,
+                penalty_weight_linear=300,
+                penalty_weight_quadratic=1,
+                penalty_weight_uniformity=1,
+                mask_margin_mm=0,
+                spacing_mm=3),
+            Optimization_Config(
+                structure_name="URETHRA",
+                dose_voxel_goal=0,
+                penalty_weight_linear=1,
+                penalty_weight_quadratic=1,
+                penalty_weight_uniformity=1,
+                mask_margin_mm=0,
+                spacing_mm=1),
+            Optimization_Config(
+                structure_name="RECTUM",
+                dose_voxel_goal=0,
+                penalty_weight_linear=1,
+                penalty_weight_quadratic=1,
+                penalty_weight_uniformity=1,
+                mask_margin_mm=0,
+                spacing_mm=3)
+        ],
         "LH": [
             Optimization_Config(
                 structure_name="CTV",
@@ -307,57 +307,57 @@ def eval_optim(
                 mask_margin_mm=0,
                 spacing_mm=3)
         ],
-        # "LQH": [
-        #     Optimization_Config(
-        #         structure_name="CTV",
-        #         dose_voxel_goal=target_dose,
-        #         penalty_weight_linear=300,
-        #         penalty_weight_quadratic=1,
-        #         penalty_weight_hotspot=1,
-        #         hotspot_threshold=1.5,
-        #         mask_margin_mm=0,
-        #         spacing_mm=3),
-        #     Optimization_Config(
-        #         structure_name="URETHRA",
-        #         dose_voxel_goal=0,
-        #         penalty_weight_linear=1,
-        #         penalty_weight_quadratic=1,
-        #         mask_margin_mm=0,
-        #         spacing_mm=1),
-        #     Optimization_Config(
-        #         structure_name="RECTUM",
-        #         dose_voxel_goal=0,
-        #         penalty_weight_linear=1,
-        #         penalty_weight_quadratic=1,
-        #         mask_margin_mm=0,
-        #         spacing_mm=3)
-        # ],
-        # "LQHU": [
-        #     Optimization_Config(
-        #         structure_name="CTV",
-        #         dose_voxel_goal=target_dose,
-        #         penalty_weight_linear=300,
-        #         penalty_weight_quadratic=1,
-        #         penalty_weight_uniformity=1,
-        #         penalty_weight_hotspot=1,
-        #         hotspot_threshold=1.5,
-        #         mask_margin_mm=0,
-        #         spacing_mm=3),
-        #     Optimization_Config(
-        #         structure_name="URETHRA",
-        #         dose_voxel_goal=0,
-        #         penalty_weight_linear=1,
-        #         penalty_weight_quadratic=1,
-        #         mask_margin_mm=0,
-        #         spacing_mm=1),
-        #     Optimization_Config(
-        #         structure_name="RECTUM",
-        #         dose_voxel_goal=0,
-        #         penalty_weight_linear=1,
-        #         penalty_weight_quadratic=1,
-        #         mask_margin_mm=0,
-        #         spacing_mm=3)
-        # ],
+        "LQH": [
+            Optimization_Config(
+                structure_name="CTV",
+                dose_voxel_goal=target_dose,
+                penalty_weight_linear=300,
+                penalty_weight_quadratic=1,
+                penalty_weight_hotspot=1,
+                hotspot_threshold=1.5,
+                mask_margin_mm=0,
+                spacing_mm=3),
+            Optimization_Config(
+                structure_name="URETHRA",
+                dose_voxel_goal=0,
+                penalty_weight_linear=1,
+                penalty_weight_quadratic=1,
+                mask_margin_mm=0,
+                spacing_mm=1),
+            Optimization_Config(
+                structure_name="RECTUM",
+                dose_voxel_goal=0,
+                penalty_weight_linear=1,
+                penalty_weight_quadratic=1,
+                mask_margin_mm=0,
+                spacing_mm=3)
+        ],
+        "LQHU": [
+            Optimization_Config(
+                structure_name="CTV",
+                dose_voxel_goal=target_dose,
+                penalty_weight_linear=300,
+                penalty_weight_quadratic=1,
+                penalty_weight_uniformity=1,
+                penalty_weight_hotspot=1,
+                hotspot_threshold=1.5,
+                mask_margin_mm=0,
+                spacing_mm=3),
+            Optimization_Config(
+                structure_name="URETHRA",
+                dose_voxel_goal=0,
+                penalty_weight_linear=1,
+                penalty_weight_quadratic=1,
+                mask_margin_mm=0,
+                spacing_mm=1),
+            Optimization_Config(
+                structure_name="RECTUM",
+                dose_voxel_goal=0,
+                penalty_weight_linear=1,
+                penalty_weight_quadratic=1,
+                mask_margin_mm=0,
+                spacing_mm=3)
+        ],
     }
 
     for pth_dicom in dir_all_dicoms:
@@ -379,7 +379,7 @@ def eval_optim(
                     config_list=config_variations[config_var],
                     package=package,
                     solver=solver,
-                    pth_out_dose=dir_all_dose_rates/f"optimized_{pth_dicom.name}_{package}_{solver}_{config_var}.nrrd",
+                    # pth_out_dose=dir_all_dose_rates/f"optimized_{pth_dicom.name}_{package}_{solver}_{config_var}.nrrd",
                 )
                     results_solver.loc[len(results_solver)] = optim_trial_result | {
                         "loading_time": t1_loading - t0_loading,
@@ -388,7 +388,7 @@ def eval_optim(
                     results_solver.to_csv(
                         dir_all_dose_rates/f"eval_optim_results_{package}.csv",
                         index=False)
-        break # for debugging only
+        # break # for debugging only
 
 if __name__ == "__main__":
     dir_all_dicoms = Path("/home/ubuntu").joinpath("YourLocalHome/Data/prostate/prostate-glen-2023")
