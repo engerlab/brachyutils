d3f0256aa285032ac5b89e27189378a4b238ff41
hosseinjafar <hosseinj.2555@gmail.com>
2025-11-26 14:35:28 -0500

    since the bottle neck for loading dose files is the hard drive, more cores do not help. i'd stick with 8

--- brachyutils/planning/plan_utils.py
+++ brachyutils/planning/plan_utils.py
@@ -591,7 +591,7 @@ class BrachyPlan:
 
         # load the dose rate tensor
         if multi_processing:                        
-            with Pool(processes=max(os.cpu_count(), 8)) as pool:
+            with Pool(processes=8 if cpu_count()>8 else 4) as pool:
                 func = partial(
                     _load_single_dose_or_uncertainty_to_dict,
                     load_dose_or_uncertainty=load_dose_or_uncertainty,
