7f67e12d783b3dec6a82ff9a8d49ea5b7bb5a748
hosseinjafar <hosseinj.2555@gmail.com>
2026-03-16 11:40:12 -0400

    benching the croped version

--- brachyutils/tests/test_dose_generation_utils.py
+++ brachyutils/tests/test_dose_generation_utils.py
@@ -96,6 +96,18 @@ def test_DoseMC():
 def test_run_dose_gen_tg43():
     dir_dicom = Path("data_test/prostate-glen-p1-dcm").resolve()
     dir_export = Path("temp_data/tg43/RapidBrachyTG43")/dir_dicom.stem
+    export_config = {
+        "dir_export": dir_export,
+        "export_config_egsphant": {
+            "strict_name_match": False,
+            "crop_by_contour": ["ctv", "urethra", "rectum"]},
+        "export_config_macfile": {
+            "name_combined": "cropped_combined"
+            },
+        "export_config_planfile": {
+            "name_combined": "cropped_combined"
+        }
+        }
     plan_obj = get_a_plan(
         dir_dicom=dir_dicom,
         from_delivered_dwellpositions=False,
@@ -103,14 +115,16 @@ def test_run_dose_gen_tg43():
     )
     t0=time()
     dose_gen = RapidBrachyTG43(
-        dir_plan_export=dir_export
+        dir_plan_export=dir_export,
     )
     dose_gen.run_dose_generation(
         plan=plan_obj,
+        export_config_brachyplan=export_config,
     )
     t1=time()
     # test the case with only combined dose
     print(f"time for RapidBrachyTG43: {t1-t0}")
+    BrachyDose(dir_export/"cropped_combined.seq.nrrd").write_to_nrrd(dir_export/"cropped_combined.seq.nrrd")
 
 def test_run_brachyutilstg43():
     from brachyutils.dose.tg43_dose_calculator import BrachyUtilsTG43
@@ -194,6 +208,6 @@ if __name__ == "__main__":
 
     # print(response.json())
 
-    # test_run_dose_gen_tg43()
+    test_run_dose_gen_tg43()
     # test_run_brachyutilstg43()
-    compare_rb_bu()
+    # compare_rb_bu()
