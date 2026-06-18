685e39a6c19bb8d172c971d85792d2acca4b891b
hosseinjafar <hosseinj.2555@gmail.com>
2026-06-18 15:35:31 -0400

    1st blueprint for catheter box

--- brachyutils/geometry/catheter_utils/catheter_box.py
+++ brachyutils/geometry/catheter_utils/catheter_box.py
@@ -2,6 +2,11 @@ from brachyutils.geometry.catheter_utils import Catheter
 from pydantic import BaseModel, ConfigDict, computed_field, Field, field_validator
 from typing import Dict, List
 import numpy as np
+import trimesh
+
+
+from brachyutils.geometry.catheter_utils.catheter_table import CatheterTable
+from brachyutils.geometry.catheter_utils.config_cathgen import Config_Angled_CathGen
 
 class CatheterSegment(Catheter):
     cluster_name_id: str
@@ -50,3 +55,109 @@ class SegmentCluster(BaseModel):
     @property
     def name_id(self) -> str:
         return f"({self.depth},{self.index+1})"
+
+class CatheterBox(BaseModel):
+    r"""
+    ### Purpose:
+    - A class to represent the bounding box where candidate catheter trajectories
+    are defined.
+
+    ### Attributes:
+    - num_physical_catheters: int := the number of physical catheters to be inserted.
+    - structure_dict: Dict[str, Trimesh] := a dictionary of structures to be considered
+    for catheter trajectory optimization.
+    - rotation_angle_deg: float := the rotation angle of the catheter box around the right left (X) axis (degrees).
+    This value should be less than 15 degrees.
+    - insertion_point_spacing_mm: float := the spacing between adjacent catheter insertion
+    points on the bottom plane (mm).
+    - num_decision_planes: int := the number of decision planes to be defined in the 
+    catheter box. All insertion points and landing points must be on the decision planes.
+    At least there are 2 planes: inferior plane and superior plane.
+    - config_angle: Dict[str, Config_Angled_CathGen] | Config_Angled_CathGen | None: The angle configuartion
+    for each insertion point. If a single Config_Angled_CathGen is provided, it will be 
+    applied to all insertion points. If None, the default Config_Angled_CathGen() 
+    will be applied to all insertion points.
+    """
+    # TODO: For future consider the following concepts
+    # - insertion_point_margin_mm: float := the margin from the edge of the bottom plane to the first 
+    # insertion point (mm).
+
+    num_physical_catheters: int = Field(default=1, description="the number of physical catheters to be inserted.")
+    structure_dict: Dict[str, trimesh.Trimesh] = Field(default_factory=dict, description="a dictionary of structures to be considered for catheter trajectory optimization.")
+    rotation_angle_deg: float = Field(default=0, description="the rotation angle of the catheter box around the right left (X) axis (degrees).")
+    insertion_point_spacing_mm: float = Field(default=10, description="the spacing between adjacent catheter insertion points on the bottom plane (mm).")
+    num_decision_planes: int = Field(default=2, description="the number of decision planes to be defined in the catheter box.")
+    config_angle: Dict[str, Config_Angled_CathGen] | Config_Angled_CathGen | None = None
+    _segment_cluster_dict: Dict[str, SegmentCluster] = None
+    _cached_catheter_table: CatheterTable = None
+
+    @computed_field
+    @property
+    def catheter_table(self) -> CatheterTable:
+        r"""
+        ### Purpose:
+        - To generate a catheter table object from all the segments in the catheter box.
+        This will be used for dose rate generation and dose optimization.
+        """
+        if self._cached_catheter_table is not None:
+            return self._cached_catheter_table
+        else:
+            pass
+    
+    def get_colliding_segments(self) -> Dict[str, List[str]]:
+        r"""
+        ### Purpose:
+        - To generate a dictionary of colliding segments for each segment in the catheter box.
+        The key is the segment name_id, and the value is a list of name_ids of segments that collide with it.
+        """
+        pass
+
+    def get_segments_at_depth(self, depth: int) -> Dict[str, CatheterSegment]:
+        r"""
+        ### Purpose:
+        - To get all the segments at a specific depth in the catheter box.
+        The depth of the root cluster is 0, the depth of its children is 1, and so on.
+        """
+        pass
+    
+    def get_parent_segments(self, segment_name_id: str) -> Dict[str, CatheterSegment]:
+        r"""
+        ### Purpose:
+        - To get all the parent segments of a specific segment in the catheter box.
+        The parent segments are the segments that are on the same chain of segments
+        leading to the root cluster.
+        """
+        pass
+    
+    def get_geometric_constraints(self) -> Dict:
+        r"""
+        ### Purpose:
+        - To generate the geometric constraints for catheter trajectory optimization.
+        The geometric constraints include the following:
+        - Uniqueness constraint: only one segment from each insertion point can be selected.
+        $$
+            \sum_k c_k = 1\\
+            \forall c_k \textrm{ with the same insertion point }
+        $$
+        
+        - Continuity Constraint: If a segment from an inner decision plane is selected, all its
+        parents on the segment chain must be selected.
+        $$
+            e_F (\sum_k^{F-1} c_k) = (F-1) c_{F} \\
+            \forall c_k \textrm{ on the same chain of segments with length of F}, \quad e_F \in \{0,1\}
+        $$
+        
+        - Collision Constraint: If a segment is selected, all segments that collide with it must not be selected.
+        $$
+            \sum_k c_k = 1\\
+            \forall c_k \textrm{ in a collision cluster}\\
+        $$
+        
+        - Catheter Number Constraint: The total number of selected segments with depth of 0 must 
+        be less than equal to the number of physical catheters to be inserted.
+        $$
+            \sum_k c_k \leq num_physical_catheters\\
+            \forall c_k \textrm{ with depth of 0}
+        $$
+        """
+        pass
