fff23308ebb0fdbcd2fd701d7643ca37aaca10b1
hosseinjafar <hosseinj.2555@gmail.com>
2026-06-24 14:19:41 -0400

    decided to turn the plane dictionary into a proper object for clarity

--- brachyutils/geometry/catheter_utils/config_cathgen.py
+++ brachyutils/geometry/catheter_utils/config_cathgen.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field
-
+from numpy.typing import ArrayLike
+from typing import List, Tuple
 class Config_Angled_CathGen(BaseModel):
     """
     ### Purpose:
@@ -19,3 +20,26 @@ sweeps -x_angle_max to +x_angle_max. 0 means parallel to normal, 90 means perpen
     y_angle_max: float = Field(default=10, description="half-width angle away from normal rotating around y axis (degrees); \
 sweeps -y_angle_max to +y_angle_max.")
     y_angle_step: float = Field(default=10, description="anterior-posterior (ap) angle increment (degrees)")
+
+class Decision_Plane(BaseModel):
+    r"""
+    ### Purpose:
+    - An object to store decision plane attributes. This will be returned by obb_planes()
+    
+    ### Attributes:
+    depth: int := The order of this decision plane. starts from zero.
+    origin: ArrayLike:= The center of the decision plane in world coordinates. len = 3.
+    normal: ArrayLike:= The normal of the decision plane in world coordinates. len = 3.
+    transform: ArrayLike:= The transform from matrix between world axis to the plane's local axis.
+    size 4 x 4
+    extents: ArrayLike:= The full length of the sides of the plane.
+    """
+    depth: int = Field(..., description="The order of this decision plane. starts from zero.")
+    origin: ArrayLike = Field(..., description="The center of the decision plane in \
+world coordinates. len = 3.")
+    normal: ArrayLike = Field(..., description="The normal of the decision plane in world coordinates. len = 3.")
+    transform: ArrayLike = Field(..., description="The transform from matrix between world axis to the plane's local axis.\
+size 4 x 4")
+    extents: ArrayLike = Field(..., description="The full length of the sides of the plane.")
+    segment_lines: List[Tuple] = Field(default=None, description="The list of the segments lines that departure \
+from this plane.")
